9ddc5a7bdc0397e78cee5aa5fabb5bb8403be1c0
troyfoster <troy@troyfoster.net>
2024-04-10 20:20:43 -0700

    Update streamlit_app.py
    Expand JSON response from Fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,6 @@ if ingredients_list:
 
     # New section to display fruityvice nutrition informatiopn
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response)
+    # st.text(fruityvice_response)
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
